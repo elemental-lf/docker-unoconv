ff49e93932a540526c61f4d2d833c592eddbc606
Lars Fenneberg <lf@elemental.net>
2019-04-30 15:02:37 +0200

    Remove Minio client containers after use

--- tests/create-buckets.sh
+++ tests/create-buckets.sh
@@ -7,7 +7,7 @@ function mc
   port=$1; shift
   command="$@"
   
-  docker run -it --net=host --entrypoint=/bin/sh minio/mc -c "/usr/bin/mc -q --insecure config host add s3 http://localhost:$port/ minio minio123; /usr/bin/mc -q --insecure $command"
+  docker run -it --rm --net=host --entrypoint=/bin/sh minio/mc -c "/usr/bin/mc -q --insecure config host add s3 http://localhost:$port/ minio minio123; /usr/bin/mc -q --insecure $command"
 }
 
 mc 9000 mb s3/unoconv-input
